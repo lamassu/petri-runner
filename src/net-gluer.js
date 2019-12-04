6817bf0bd8802939095f435bd9ebf1eafacb4685
Josh Harvey <josh@lamassu.is>
2019-12-04 18:59:20 +0000

    WIP

--- src/net-gluer.js
+++ src/net-gluer.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
-// const assert = require('assert')
+const assert = require('assert')
 
 const toposort = require('toposort')
 const R = require('ramda')
 
 let rootNet
 const nets = {}
+const subnetCounter = {}
 
 function bail (msg) {
   console.log(`error: ${msg}`)
@@ -25,6 +26,7 @@ function loadNet (net) {
   const initialPlace = initialPlaces[0]
 
   nets[net.name] = net
+  subnetCounter[net.name] = 0
 
   if (R.includes('root', initialPlace.tags)) {
     rootNet = net
@@ -44,18 +46,35 @@ function computeSubnetArcs (parentNet) {
   return R.map(R.pair(parentNetName), subnetNames)
 }
 
+function expandWith (parentNet, subnetName) {
+  const subnet = nets[subnetName]
+  assert(subnet, 'No such subnet')
+
+  const subnetTerminalTransitionNames = R.sortBy(R.identity, R.map(R.prop('name'), R.filter(r => R.isEmpty(r.outputs), subnet.transitions)))
+  const parentCollapsedTransitionNames =
+    R.sortBy(R.identity, R.map(R.prop('name'), R.filter(r => r.inputs.length === 1 && R.head(r.inputs).srcPlace))
+  // Need to check lots of things but return only specific things, so this style isn't great for that
+  // Try simple forEach loops
+
+  subnetCounter[subnetName] += 1
+}
+
+function expandNet (parentNet) {
+  const subnetNames = R.map(R.prop('name'), R.filter(placeSubnet, parentNet.places))
+  R.forEach(subnet => expandWith(parentNet, subnet), subnetNames)
+}
+
 // const consoleTap = R.tap(console.log)
 
 const filepath = process.argv[2]
 const netStructure = JSON.parse(fs.readFileSync(filepath))
 loadNets(netStructure)
 const subnetArcs = R.unnest((R.map(computeSubnetArcs, R.values(nets))))
-const netDependencies = toposort(subnetArcs)
-
+const netDependencies = R.reverse(toposort(subnetArcs))
+R.forEach(expandNet, netDependencies)
 console.log(netDependencies)
 
 /*
-* Need to compute dependency graph.
-* First create graph of net dependencies.
-* Then do a topological sort.
+* Work up dependency list from the bottom, gluing along the way.
+* Be careful about namespacing and separately namespacing duplicated subnets.
 */
