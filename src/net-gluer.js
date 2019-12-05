ef0de01bb9a8ffe074bd7819a422379e87dae221
Josh Harvey <josh@lamassu.is>
2019-12-05 11:50:09 +0000

    WIP

--- src/net-gluer.js
+++ src/net-gluer.js
@@ -5,6 +5,8 @@ const toposort = require('toposort')
 const R = require('ramda')
 
 let rootNet
+let globalCount = 0
+
 const nets = {}
 const subnetCounter = {}
 
@@ -46,17 +48,78 @@ function computeSubnetArcs (parentNet) {
   return R.map(R.pair(parentNetName), subnetNames)
 }
 
-function expandWith (parentNet, subnetName) {
-  const subnet = nets[subnetName]
-  assert(subnet, 'No such subnet')
+function expandWith (parentNet, subnetPlaceName) {
+  const am = s => `${parentName} <- ${subnetPlaceName}: ${s}`
+  const subnet = nets[subnetPlaceName]
+  const parentName = parentNet.name
+  assert(subnet, am('No such subnet.'))
+  const isInitialPlace = R.pipe(R.prop('tags'), R.includes('initial'))
+  const subnetInitialPlaces = R.filter(isInitialPlace, subnet.places)
+  assert(subnetInitialPlaces.length === 1, am("Subnet doesn't have single initial place"))
+  const subnetInitialPlace = R.head(subnetInitialPlaces)
+  assert(subnetInitialPlace.name === subnetPlaceName, am('Subnet initial place incorrectly named.'))
+  const toSortedTransitionNames = R.pipe(R.map(R.prop('name')), R.sortBy(R.identity))
+  const isTerminalTransition = r => R.isEmpty(r.outputs)
+  const subnetTerminalTransitions = R.filter(isTerminalTransition, subnet.transitions)
+  const subnetTerminalTransitionNames = toSortedTransitionNames(subnetTerminalTransitions)
 
-  const subnetTerminalTransitionNames = R.sortBy(R.identity, R.map(R.prop('name'), R.filter(r => R.isEmpty(r.outputs), subnet.transitions)))
-  const parentCollapsedTransitionNames =
-    R.sortBy(R.identity, R.map(R.prop('name'), R.filter(r => r.inputs.length === 1 && R.head(r.inputs).srcPlace))
-  // Need to check lots of things but return only specific things, so this style isn't great for that
-  // Try simple forEach loops
+  const isInitialTransition = R.pipe(R.prop('inputs'), R.includes(subnetPlaceName))
+  const collapsedTransitions = R.filter(transitionFromSubnetPlace, parentNet.transitions)
+  const singleInput = R.pipe(R.prop('inputs'), R.length, R.equals(1))
+  assert(R.all(singleInput, collapsedTransitions),
+    am('Has collapsed transition with multiple inputs.'))
+  const collapsedTransitionNames = toSortedTransitionNames(collapsedTransitions)
+  assert(R.equals(subnetTerminalTransitionNames, collapsedTransitionNames),
+    am('Collapsed transitions differ from subnet terminal transitions.'))
 
-  subnetCounter[subnetName] += 1
+  // parent places remain the same
+  // add namespaced subplaces, except for initial place
+  const subnetNonInitialPlaces = R.reject(isInitialPlace, subnet.places)
+  const namespaceSubnet = name => `${name}_${globalCount}`
+  const subnetPlaces = R.map(namespaceSubnet, subnetNonInitialPlaces)
+  parentNet.places = R.concat(parentNet.places, subnetPlaces)
+
+  const initialTransitions = R.filter(isInitialTransition, subnet.transitions)
+  assert(R.all(singleInput, initialTransitions),
+    am('Subnet has initial transition with multiple inputs.'))
+
+  const collapsedTransitionLookup = R.fromPairs(R.map(t => [t.name, t], collapsedTransitions))
+
+  const computeOutputs = t => {
+    if (isTerminalTransition(t)) {
+      const parentTransition = collapsedTransitionLookup[t.name]
+      assert(parentTransition, 'No such collapsed transition in parent.')
+      return parentTransition.outputs
+    }
+
+    return R.map(namespaceSubnet, t.outputs)
+  }
+
+  const computeInputs = t => {
+    if (isInitialTransition(t)) {
+      const input = R.head(t.inputs)
+      assert(input.srcTokenCount === 1, am(`[${t.name}] Initial transition input token count is not 1.`))
+      return [{ srcPlace: subnetPlaceName, srcTokenCount: 1 }]
+    }
+
+    return R.map(namespaceSubnet, t.inputs)
+  }
+
+  const transformSubnetTransition = t => {
+    return {
+      name: isTerminalTransition(t) ? t.name : namespaceSubnet(t),
+      tags: t.tags,
+      inputs: computeInputs(t),
+      outputs: computeOutputs(t)
+    }
+  }
+
+  const subnetTransitions = R.map(transformSubnetTransition, subnet.transitions)
+  parentNet.transitions = R.concat(parentNet.transitions, subnetTransitions)
+  globalCount++
+
+  // What about numbered subnets (more than one call to a subnet in a parent net, so it has to be numbered). We need to
+  // recognize the subnet and handle it properly.
 }
 
 function expandNet (parentNet) {
